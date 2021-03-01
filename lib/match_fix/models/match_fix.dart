625c0332fa402a94e3dba3506bc217206e80969c
johnzdictator <yohannesketemazeleke@gmail.com>
2021-03-02 00:39:48 +0300

    match_fix models

---
+++ lib/match_fix/models/match_fix.dart
@@ -0,0 +1,34 @@
+import 'package:equatable/equatable.dart';
+import 'package:soccer_frontend/club/models/models.dart';
+import 'package:soccer_frontend/stadium/models/stadium.dart';
+import 'package:meta/meta.dart';
+
+class MatchFix extends Equatable {
+  final String id;
+  final List<Club> clubs;
+  final DateTime fixDate;
+  final Stadium stadium;
+
+  MatchFix({
+    @required this.id,
+    @required this.clubs,
+    @required this.fixDate,
+    @required this.stadium,
+  });
+
+  @override
+  List<Object> get props => [id, clubs, fixDate, stadium];
+
+  factory MatchFix.fromJson(Map<String, dynamic> json) {
+    return MatchFix(
+      id: json['id'],
+      clubs: json['clubs'],
+      fixDate: json['fixDate'],
+      stadium: json['stadium'],
+    );
+  }
+
+  @override
+  String toString() =>
+      'MatchFix id: $id, clubs: $clubs, fixDate: $fixDate, stadium: $stadium';
+}
---
+++ lib/match_fix/models/models.dart
@@ -0,0 +1,1 @@
+export 'package:soccer_frontend/match_fix/models/match_fix.dart';
